235df75f236a227d6609625cded8fd40c1b9b698
Pepe Federico <pepe.00.fp@outlook.it>
2025-03-29 10:25:40 +0100

    Dimostrazione Cambio, aggiunto ;

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("this is my first application")
+        System.out.println("this is my first application");
     }
 }
